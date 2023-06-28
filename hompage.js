f686b8d38cdb7b97037c21669048d99bfab311f7
Grocky94 <lastleftdefence@gmail.com>
2023-06-29 01:05:36 +0530

    update

--- hompage.js
+++ hompage.js
@@ -3,27 +3,58 @@ let verified = document.getElementById("Verified");
 let showFilter = document.getElementById("filter-box")
 let page = document.getElementById("screen")
 let cross = document.getElementById("cross")
+let more = document.getElementById("clickMore");
 
-verified.addEventListener('click', function open() {
-    // alert('clicked');
+document.addEventListener("DOMContentLoaded", function () {
 
-    if (showFilter.style.display == "none") {
-        showFilter.style.display = "block"
+    verified.addEventListener('click', function open() {
+        // alert('clicked');
 
-    } else {
-        showFilter.style.display = "none"
+        if (showFilter.style.display === "none") {
+            showFilter.style.display = "block"
 
-    }
-})
+        } else {
+            showFilter.style.display = "none"
+
+        }
+    })
+
+    cross.addEventListener('click', function close() {
+
+        if (showFilter.style.display === "block") {
+            showFilter.style.display = "none"
+        } else {
+            showFilter.style.display = "block"
+        }
+    })
 
 
+    more.addEventListener("pointerdown", function pop() {
+        if (moreBox.style.display === "none") {
+            moreBox.style.display = "block"
+        } else {
+            moreBox.style.display = "none"
+        }
+    })
+let moreBox = document.getElementById("more-div");
 
-cross.addEventListener('click', function close() {
+    document.addEventListener("click", function moveOut(e) {
+        if (e.target !== more) {
+            moreBox.style.display = "none"
+        }else {
+            moreBox.style.display = "block"
+        }
+    })
 
-    if (showFilter.style.display = "block") {
-        showFilter.style.display = "none"
-    } else {
-        showFilter.style.display = "block"
-
-    }
-})
+    let draftBtn = document.getElementById("draftBtn")
+    let draftBox = document.getElementById("draft")
+    draftBtn.addEventListener("click", function dft(e) {
+        if (draftBtn.contains(e.target)) {
+            if (draftBox.style.display === "none") {
+                draftBox.style.display = "block";
+            } else {
+                draftBox.style.display = "none";
+            }
+        }
+    });
+});
