eb719a8ae484f205a6dea452b8b3904b6d99d237
Grocky94 <lastleftdefence@gmail.com>
2023-07-10 08:54:57 +0530

    update

--- hompage.js
+++ hompage.js
@@ -1,60 +1,105 @@
-
 let verified = document.getElementById("Verified");
-let showFilter = document.getElementById("filter-box")
-let page = document.getElementById("screen")
-let cross = document.getElementById("cross")
+let showFilter = document.getElementById("filter-box");
+let page = document.getElementById("screen");
+let cross = document.getElementById("cross");
 let more = document.getElementById("clickMore");
+let moreBox = document.getElementById("more-div");
+let draftBtn = document.getElementById("draftBtn");
+let draftBox = document.getElementById("draft");
+let profBtn = document.getElementById("prof-tool");
+let profBox = document.getElementById("addition-child-more-small-div");
+let profTool = document.getElementById("twitter-for-professional");
+let profpopup = document.getElementById("Professional-box");
+let profpopupClose = document.getElementById("x");
+let middleArea = document.getElementById("middle-body");
+let rightArea = document.getElementById("right-body");
 
+
+// linstner to load all together
 document.addEventListener("DOMContentLoaded", function () {
 
-    verified.addEventListener('click', function open() {
-        // alert('clicked');
 
-        if (showFilter.style.display === "none") {
-            showFilter.style.display = "block"
-
-        } else {
-            showFilter.style.display = "none"
-
-        }
-    })
-
-    cross.addEventListener('click', function close() {
-
+    // for closeing verify popup screen 
+    cross.addEventListener('click', function () {
         if (showFilter.style.display === "block") {
-            showFilter.style.display = "none"
+            showFilter.style.display = "none";
         } else {
-            showFilter.style.display = "block"
-        }
-    })
-
-
-    more.addEventListener("pointerdown", function pop() {
-        if (moreBox.style.display === "none") {
-            moreBox.style.display = "block"
-        } else {
-            moreBox.style.display = "none"
-        }
-    })
-let moreBox = document.getElementById("more-div");
-
-    document.addEventListener("click", function moveOut(e) {
-        if (e.target !== more) {
-            moreBox.style.display = "none"
-        }else {
-            moreBox.style.display = "block"
-        }
-    })
-
-    let draftBtn = document.getElementById("draftBtn")
-    let draftBox = document.getElementById("draft")
-    draftBtn.addEventListener("click", function dft(e) {
-        if (draftBtn.contains(e.target)) {
-            if (draftBox.style.display === "none") {
-                draftBox.style.display = "block";
-            } else {
-                draftBox.style.display = "none";
-            }
+            showFilter.style.display = "block";
         }
     });
+
+    // function made to stop default property 
+    moreBox.addEventListener("click", function (e) {
+        e.stopPropagation();// use to not trigger screen when call child listner
+    });
+
+    document.addEventListener("click", function () {
+        moreBox.style.display = "none";
+    });
+
+   
+
 });
+
+// for verify 
+verified.addEventListener('click', function () {
+    if (showFilter.style.display === "none") {
+        showFilter.style.display = "block";
+    } else {
+        showFilter.style.display = "none";
+    }
+})
+// made for more btn listner 
+more.addEventListener("click", function (e) {
+    e.stopPropagation();//use to not trigger child listner
+    if (getComputedStyle(moreBox).display === "none") {
+        moreBox.style.display = "block";
+        profBox.style.display = "none";
+    } else {
+        moreBox.style.display = "none";
+    }
+});
+
+// listner for draft 
+draftBtn.addEventListener("click", function (e) {
+    if (draftBtn.contains(e.target)) {
+        if (draftBox.style.display === "none") {
+            draftBox.style.display = "block";
+        } else {
+            draftBox.style.display = "none";
+        }
+    }
+});
+
+// listner for profession 
+profBtn.addEventListener("click", function () {
+    if (getComputedStyle(profBox).display === "none") {
+        profBox.style.display = "block";
+    } else {
+        profBox.style.display = "none";
+    }
+});
+// listner for professional inner child 
+profTool.addEventListener("click", function (e) {
+    e.stopPropagation();
+    if (e.currentTarget === profTool) {
+        profpopup.style.display = "block"
+        middleArea.style.position = 'fixed';
+        rightArea.style.position = 'fixed';
+    } else {
+        profpopup.style.display = "none"
+    }
+})
+profpopupClose.addEventListener("click", function (e) {
+    if (e.currentTarget === profpopupClose) {
+        console.log(e.target)
+        profpopup.style.display = "none"
+        middleArea.style.position = 'relative';
+        rightArea.style.position = 'relative';
+    } else {
+        profpopup.style.display = "block"
+        middleArea.style.position = 'fixed';
+        rightArea.style.position = 'fixed';
+
+    }
+})
